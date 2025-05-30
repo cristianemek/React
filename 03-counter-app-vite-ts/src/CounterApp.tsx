241cf237f61bfae18e3ae7cd3646ef6eeaeaff19
USUARIO\crist <e>
2025-05-30 19:33:13 +0200

    feauture:buttons y fin seccion 4

--- 03-counter-app-vite-ts/src/CounterApp.tsx
+++ 03-counter-app-vite-ts/src/CounterApp.tsx
@@ -1,31 +1,35 @@
 import { useState } from "react";
 
-
 interface CounterAppProps {
-    value: number;
+  value: number;
 }
 
-export const CounterApp = ({value}: CounterAppProps) => {
-    
-    const [counter,setCounter] = useState(value);
+export const CounterApp = ({ value }: CounterAppProps) => {
+  const [counter, setCounter] = useState(value);
 
-    const handleAdd = (type:"increase"|"decrease") => {
-        if (type==="increase") {
-            setCounter(counter+1);
-        }else{
-            setCounter(counter-1);
-        }
-    }
+//   const handleChange = (type?: "increase" | "decrease") => {
+//     if (type === "increase") {
+//       setCounter(counter + 1);
+//     } else if (type === "decrease") {
+//       setCounter(counter - 1);
+//     } else {
+//       setCounter(value);
+//     }
+//   };
 
-    return ( 
-        <>
-            <h1>Counter App</h1>
-            <h2> { counter } </h2>
-            <button onClick={()=>handleAdd("increase")}>+1</button>
-            <button onClick={()=>handleAdd("decrease")}>-1</button>
+const handleIncrease = () => setCounter(counter + 1);
+const handleDecrease = () => setCounter(counter - 1);
+const handleReset = () => setCounter(value);
 
-        </>
-     );
-}
+  return (
+    <>
+      <h1>Counter App</h1>
+      <h2> {counter} </h2>
+      <button onClick={() =>handleIncrease()}>+1</button>
+      <button onClick={() => handleDecrease()}>-1</button>
+      <button onClick={() =>handleReset()}>reset</button>
+    </>
+  );
+};
 
 export default CounterApp;
