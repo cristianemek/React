519d5c339de805de0eda45151f6a3e03d4ca8680
Cristian <cristianrodriguezlugo@gmail.com>
2025-06-13 08:10:15 +0200

    avance mostrar gifs

--- 04-gif-expert-app/src/components/GifGrid.tsx
+++ 04-gif-expert-app/src/components/GifGrid.tsx
@@ -1,22 +1,41 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { getGifs } from "../helpers/getGifs"
 
 interface GridProps{
     category:string
 }
 
+interface GifImage {
+  id: string;
+  title: string;
+  // add other properties if needed
+}
+
 export const GifGrid = ({category}:GridProps) => {
 
+  const [images, setImages] = useState<GifImage[]>([])
   
+  const getImages = async() =>{
+    const newImages = await getGifs(category);
+    setImages(newImages);
+
+  }
+
   useEffect(() => {
-    getGifs(category);
-  }, [])
+    getImages();
+  }, [category])
   
 
   return (
     <>
         <h3>{category}</h3>
-        <p>Hola mundo</p>
+        <ol>
+          {
+            images.map( img => (
+              <li key={img.id}>{img.title}</li>
+            ))
+          }
+        </ol>
     </>
   )
 }
--- 04-gif-expert-app/src/helpers/getGifs.ts
+++ 04-gif-expert-app/src/helpers/getGifs.ts
@@ -1,14 +1,13 @@
 export const getGifs = async (category: string) => {
-  const url = `api.giphy.com/v1/gifs/search?api_key=uPidyhBsiCl8hWc784ZWaCxEd5tX9Oc6&q=${category}&limit=10`;
-  const resp = await fetch(url);
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=uPidyhBsiCl8hWc784ZWaCxEd5tX9Oc6&q=${category}&limit=10`;
+    const resp = await fetch(url);
+    const { data } = await resp.json();
+    const gifs = data.map((img: any) => ({
+      id: img.id,
+      title: img.title,
+      url: img.images.downsized_medium.url
+    }));
+    
+    return gifs;
 
-  const { data } = await resp.json();
-
-  const gifs = data.map((img: any) => ({
-    id: img.id,
-    title: img.title,
-    url: img.images.downsized_medium.url,
-  }));
-
-  return gifs;
 };
