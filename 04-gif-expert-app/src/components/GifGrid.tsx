129e73982ecb26ffa39ac512c7941c0835c47588
USUARIO\crist <e>
2025-06-15 23:50:07 +0200

    casa

--- 04-gif-expert-app/src/components/GifGrid.tsx
+++ 04-gif-expert-app/src/components/GifGrid.tsx
@@ -1,19 +1,21 @@
 import { useEffect, useState } from "react";
 import { getGifs } from "../helpers/getGifs"
+import { GifItem } from "./GifItem";
 
 interface GridProps{
     category:string
 }
 
-interface GifImage {
-  id: string;
-  title: string;
+export interface GifImageProps {
+  id?:string,
+  title?:string,
+  url:string,
   // add other properties if needed
 }
 
 export const GifGrid = ({category}:GridProps) => {
 
-  const [images, setImages] = useState<GifImage[]>([])
+  const [images, setImages] = useState<GifImageProps[]>([])
   
   const getImages = async() =>{
     const newImages = await getGifs(category);
@@ -29,13 +31,17 @@ export const GifGrid = ({category}:GridProps) => {
   return (
     <>
         <h3>{category}</h3>
-        <ol>
+        <div className="card-grid">
           {
             images.map( img => (
-              <li key={img.id}>{img.title}</li>
+              <>
+                <p>{img.url}</p>
+                <GifItem key={img.id} url={img.url}/>
+              </>
             ))
           }
-        </ol>
+        </div>
+
     </>
   )
 }
---
+++ 04-gif-expert-app/src/components/GifItem.tsx
@@ -0,0 +1,15 @@
+
+import type { GifImageProps } from './GifGrid';
+
+export const GifItem = ({url}:GifImageProps) => {
+
+console.log()
+   
+;
+    return (
+    <div className=''>
+        <img src={url} />
+        <p></p>
+    </div>
+  )
+}
