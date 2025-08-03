bb0c6f2e9713046c5e1dbc94878939b6e3f020a7
USUARIO\crist <e>
2025-08-03 11:20:50 +0200

    customHook, useRef, Caché

--- 03-gifs-app/src/GifsApp.tsx
+++ 03-gifs-app/src/GifsApp.tsx
@@ -2,31 +2,11 @@ import { CustomHeader } from "./components/CustomHeader";
 import { CustomSearch } from "./components/CustomSearch";
 import { GifList } from "./gifs/components/GifList";
 import { GifsPreviousSearches } from "./gifs/components/GifsPreviousSearches";
-import { useState } from "react";
-import { getGifsByQuery } from "./gifs/actions/get-gifs-by-query.actions";
-import type { Gif } from "./gifs/interfaces/gif.interface";
+import { useGifs } from "./gifs/hooks/useGifs";
 
 export const GifsApp = () => {
-  const [previousTerms, setPreviousTerms] = useState<string[]>([]);
-  const [gifs, setGifs] = useState<Gif[]>([]);
 
-  const handleTermClicked = async (term: string) => {
-    const gifs= await getGifsByQuery(term);
-    setGifs(gifs);
-  };
-
-  
-  const handleSearch = async (query: string) => {
-    const cleanText = query.trim().toLowerCase();
-
-    if (cleanText.length === 0 || previousTerms.includes(cleanText)) return;
-
-    setPreviousTerms([cleanText, ...previousTerms.splice(0, 3)]);
-
-    const gifs= await getGifsByQuery(query);
-    setGifs(gifs);
-  };
-
+  const {handleSearch, previousTerms,handleTermClicked,gifs } = useGifs();
   return (
     <>
       {/* Header */}
