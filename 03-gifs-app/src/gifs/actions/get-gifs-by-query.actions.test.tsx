2ba70151c20f1a9f8b6ac3d776dc9e63a095b6fc
USUARIO\crist <e>
2025-08-11 18:10:48 +0200

    axios testing

--- 03-gifs-app/src/gifs/actions/get-gifs-by-query.actions.test.tsx
+++ 03-gifs-app/src/gifs/actions/get-gifs-by-query.actions.test.tsx
@@ -1,22 +1,75 @@
-import { describe, expect, test } from "vitest";
+import { beforeEach, describe, expect, expectTypeOf, test, vi } from "vitest";
 import { getGifsByQuery } from "./get-gifs-by-query.actions";
+import AxiosMockAdapter from "axios-mock-adapter";
+import { giphyApi } from "../api/giphy.api";
+import { giphyResponseMock } from "../../mocks/giphy.response.data";
 
 
 describe('get gifs by query actions', () => {
+    let mock = new AxiosMockAdapter(giphyApi);
   
-    test('should return a list of gids', async () => { 
+    // test('should return a list of gids', async () => { 
+
+    //     const gifs = await getGifsByQuery('goku');
+    //     const [gif1]= gifs;
+
+    //     expect(gifs.length).toBe(10);
+        
+    //     expect(gif1).toEqual({
+    //         id:expect.any(String),
+    //         height:expect.any(Number),
+    //         width:expect.any(Number),
+    //         title:expect.any(String),
+    //         url:expect.any(String),
+    //     });
+    //  })
+
+    beforeEach(()=>{
+        mock = new AxiosMockAdapter(giphyApi);
+    })
+
+    test('should return a list of gifs', async() => { 
+        mock.onGet('/search').reply(200,giphyResponseMock);
 
         const gifs = await getGifsByQuery('goku');
-        const [gif1]= gifs;
-
         expect(gifs.length).toBe(10);
-        
-        expect(gif1).toEqual({
-            id:expect.any(String),
-            height:expect.any(Number),
-            width:expect.any(Number),
-            title:expect.any(String),
-            url:expect.any(String),
+
+
+        gifs.forEach(gif=>{
+            expect(typeof gif.id).toBe('string');
+            expect(typeof gif.url).toBe('string');
+            expect(typeof gif.title).toBe('string');
+            expect(typeof gif.width).toBe('number');
+            expect(typeof gif.height).toBe('number');            
         });
+    });
+
+    test('should return an empty list of gifs if q is empty', async() => { 
+        // mock.restore();
+
+        const gifs = await getGifsByQuery('');
+        expect(gifs.length).toBe(0);
+    });
+
+
+    test('should handle error when the api returns an error', async() => { 
+
+        const consoleErrorSpy= vi.spyOn(console,'error')
+            .mockImplementation(()=>{})
+
+        mock.onGet('/search').reply(400,{
+            data:{
+                message:'Bad request'
+            }
+        });
+
+        const gifs = await getGifsByQuery('goku');
+
+        expect(gifs.length).toBe(0);
+        expect(consoleErrorSpy).toHaveBeenCalled();
+        expect(consoleErrorSpy).toHaveBeenCalledTimes(1);
+
+        expect(consoleErrorSpy).toHaveBeenCalledWith(expect.anything());
+
      })
 })
