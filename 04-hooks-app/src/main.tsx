2e2e61d1b9d4fe74214b883ec57e788eea78547b
USUARIO\crist <e>
2025-08-15 19:54:57 +0200

    TrafficLightWithHook

--- 04-hooks-app/src/main.tsx
+++ 04-hooks-app/src/main.tsx
@@ -2,11 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 // import { HooksApp } from './HooksApp';
-import { TrafficLight } from './01-useState/TrafficLight';
+// import { TrafficLight } from './01-useState/TrafficLight';
+import { TrafficLightWithEffect } from './02-useEffect/TrafficLightWithEffect';
+import { TrafficLightWithHook } from './02-useEffect/TrafficLightWithHook';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <TrafficLight />
+    {/* <TrafficLight /> */}
     {/* <HooksApp /> */}
+    {/* <TrafficLightWithEffect/> */}
+    <TrafficLightWithHook />
   </StrictMode>,
 )
