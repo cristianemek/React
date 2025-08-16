335974ae0e81a8bd402695cf12cd9df96cf83535
USUARIO\crist <e>
2025-08-16 22:54:33 +0200

    useState cambiado por useReducer

--- 04-hooks-app/src/05-useReducer/reducer/tasksReducer.ts
+++ 04-hooks-app/src/05-useReducer/reducer/tasksReducer.ts
@@ -16,6 +16,16 @@ export type TaskAction =
   | { type: "TOGGLE_TODO"; payload: number }
   | { type: "DELETE_TODO"; payload: number };
 
+
+  export const getTasksInitialState=(): TaskState=>{
+    return{
+      todos: [],
+      completed: 0,
+      pending:0,
+      length:0
+    }
+  }
+
 export const taskReducer = (
   state: TaskState,
   action: TaskAction
@@ -24,7 +34,7 @@ export const taskReducer = (
     case "ADD_TODO": {
       const newTodo: Todo = {
         id: Date.now(),
-        text: action.payload.trim(),
+        text: action.payload,
         completed: false,
       };
 
