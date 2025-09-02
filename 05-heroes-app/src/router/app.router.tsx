abfc5e2d9a7ce03abbcfc7a8c79175619df7dd9f
USUARIO\crist <e>
2025-09-02 10:30:05 +0200

    Añadir estructura básica de enrutamiento y páginas para la aplicación de héroes

---
+++ 05-heroes-app/src/router/app.router.tsx
@@ -0,0 +1,39 @@
+import { createBrowserRouter } from "react-router";
+
+import { AdminPage } from "@/admin/pages/AdminPage";
+import { AdminLayout } from "@/admin/pages/layouts/AdminLayout";
+import { HeroesLayout } from "@/heroes/layouts/HeroesLayout";
+import { HeroPage } from "@/heroes/pages/hero/HeroPage";
+import { HomePage } from "@/heroes/pages/HomePage";
+import { SearchPage } from "@/heroes/pages/search/SearchPage";
+
+export const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <HeroesLayout />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: "heroes/1",
+        element: <HeroPage />,
+      },
+      {
+        path: "search",
+        element: <SearchPage />,
+      },
+    ],
+  },
+  {
+    path: "/admin",
+    element: <AdminLayout />,
+    children: [
+      {
+        index:true,
+        element: <AdminPage />,
+      },
+    ],
+  },
+]);
