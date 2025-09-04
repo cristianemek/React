6e030be144437c416202a24ec96ce2f4839860e5
USUARIO\crist <e>
2025-09-04 20:02:52 +0200

    feat: add HeroStatCard and HeroStats components for displaying hero statistics
    - Created HeroStatCard component for reusable stat card layout.
    - Implemented HeroStats component to display various hero statistics.
    - Updated HomePage to utilize HeroStats for a cleaner layout.
    - Refactored SearchPage to include HeroStats for consistent stats display.
    - Added SearchControls component for search and filter functionalities.

--- 05-heroes-app/src/heroes/pages/search/SearchPage.tsx
+++ 05-heroes-app/src/heroes/pages/search/SearchPage.tsx
@@ -1,14 +1,21 @@
 import { CustomJumbotron } from "@/components/custom/CustomJumbotron";
+import { HeroStats } from "@/heroes/components/HeroStats";
+import { SearchControls } from "./ui/SearchControls";
 
 export const SearchPage = () => {
   return (
     <>
-     <CustomJumbotron 
-            title="Búsqueda de SuperHéroes"
-            description="Descrube, explora y administra super héroes y villanos"
-           />
-           </>
-  )
-}
+      <CustomJumbotron
+        title="Búsqueda de SuperHéroes"
+        description="Descrube, explora y administra super héroes y villanos"
+      />
+      {/* Stats DashBoard */}
+      <HeroStats />
+
+      {/* Filter and search */}
+      <SearchControls />
+    </>
+  );
+};
 
 export default SearchPage;
