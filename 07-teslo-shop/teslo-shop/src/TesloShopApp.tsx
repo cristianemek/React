8679dd8f5f98b9dc75050b051242db211795a233
Cristianemek <cristianrodriguezlugo@gmail.com>
2025-09-15 23:01:15 +0200

    backend

--- 07-teslo-shop/teslo-shop/src/TesloShopApp.tsx
+++ 07-teslo-shop/teslo-shop/src/TesloShopApp.tsx
@@ -1,8 +1,15 @@
 import { RouterProvider } from "react-router"
 import { appRouter } from "./app.router"
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
+const queryClient = new QueryClient();
 
 export const TesloShopApp = () => {
   return (
-    <RouterProvider router={appRouter} />
+    <QueryClientProvider client={queryClient}>
+      {/* The rest of your application */}
+      <RouterProvider router={appRouter} />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   )
 }
